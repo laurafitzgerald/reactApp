fbb7330d56c77f10482fd414ce8dab9975d86f00
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2016-02-09 15:00:46 +0000

    Initial Project Setup

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var ReactDOM = require('react-dom')
+var React = require('react')
+
+var CyclingApp = React.createClass({
+	render : function(){
+
+		<h1>Welcome to the Cycling Application</h1>
+	}
+});
+
+
+ReactDOM.render(
+	<CyclingApp />,
+	document.getElementById('mount-point'));
